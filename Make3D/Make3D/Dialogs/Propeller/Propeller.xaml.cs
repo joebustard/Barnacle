9894b11f72297e7c69705c20656cb4401017058e
joebustard <joebustard@aol.com>
2021-03-24 18:12:46 +0000

    Added some parameters to propeller

--- Make3D/Make3D/Dialogs/Propeller/Propeller.xaml.cs
+++ Make3D/Make3D/Dialogs/Propeller/Propeller.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Windows;
 using System.Windows.Media.Media3D;
+using System.Xml;
 
 namespace Make3D.Dialogs
 {
@@ -10,11 +13,222 @@ namespace Make3D.Dialogs
     /// </summary>
     public partial class Propeller : BaseModellerDialog, INotifyPropertyChanged
     {
+        private XmlDocument airFoilDoc;
+        private List<String> airfoilGroups;
+        private List<String> airfoilNames;
+        private string airFoilPath;
+        private double bladeAngle;
+
+        private double bladeLength;
+
+        private double bladeMid;
+        private double bladeRoot;
+        private double bladeTip;
+        private int numberOfBlades;
+        private string rootGroup;
+        private double rootOffset;
+        private string selectedAirfoil;
+
         public Propeller()
         {
             InitializeComponent();
             ToolName = "Propeller";
             DataContext = this;
+            NumberOfBlades = 2;
+            BladeLength = 60;
+            RootOffset = 5;
+            BladeRoot = 4;
+            BladeMid = 7;
+            BladeTip = 5;
+            BladeAngle = 45;
+            airFoilPath = AppDomain.CurrentDomain.BaseDirectory + "data\\Airfoils.xml";
+            airFoilDoc = new XmlDocument();
+            airfoilNames = new List<string>();
+            airfoilGroups = new List<string>();
+        }
+
+        public List<string> AirfoilGroups
+        {
+            get
+            {
+                return airfoilGroups;
+            }
+            set
+            {
+                if (airfoilGroups != value)
+                {
+                    airfoilGroups = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public List<string> AirfoilNames
+        {
+            get
+            {
+                return airfoilNames;
+            }
+            set
+            {
+                if (airfoilNames != value)
+                {
+                    airfoilNames = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double BladeAngle
+        {
+            get
+            {
+                return bladeAngle;
+            }
+            set
+            {
+                if (bladeAngle != value)
+                {
+                    bladeAngle = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double BladeLength
+        {
+            get
+            {
+                return bladeLength;
+            }
+            set
+            {
+                if (bladeLength != value)
+                {
+                    bladeLength = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double BladeMid
+        {
+            get
+            {
+                return bladeMid;
+            }
+            set
+            {
+                if (bladeMid != value)
+                {
+                    bladeMid = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double BladeRoot
+        {
+            get
+            {
+                return bladeRoot;
+            }
+            set
+            {
+                if (bladeRoot != value)
+                {
+                    bladeRoot = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double BladeTip
+        {
+            get
+            {
+                return bladeTip;
+            }
+            set
+            {
+                if (bladeTip != value)
+                {
+                    bladeTip = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public int NumberOfBlades
+        {
+            get
+            {
+                return numberOfBlades;
+            }
+            set
+            {
+                if (numberOfBlades != value)
+                {
+                    if (value >= 2)
+                    {
+                        numberOfBlades = value;
+                        NotifyPropertyChanged();
+                    }
+                }
+            }
+        }
+
+        public String RootGroup
+        {
+            get
+            {
+                return rootGroup;
+            }
+            set
+            {
+                if (rootGroup != value)
+                {
+                    rootGroup = value;
+                    List<string> names = new List<String>();
+                    SetProfiles(rootGroup, names);
+
+                    NotifyPropertyChanged();
+                    AirfoilNames = names;
+                }
+            }
+        }
+
+        public double RootOffset
+        {
+            get
+            {
+                return rootOffset;
+            }
+            set
+            {
+                if (rootOffset != value)
+                {
+                    rootOffset = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string SelectedAirfoil
+        {
+            get
+            {
+                return selectedAirfoil;
+            }
+            set
+            {
+                if (selectedAirfoil != value)
+                {
+                    selectedAirfoil = value;
+                    NotifyPropertyChanged();
+                    // Update();
+                }
+            }
         }
 
         public override bool ShowAxies
@@ -63,6 +277,21 @@ namespace Make3D.Dialogs
             ClearShape();
         }
 
+        private void LoadAirFoils()
+        {
+            if (File.Exists(airFoilPath))
+            {
+                airFoilDoc.Load(airFoilPath);
+                XmlNode root = airFoilDoc.SelectSingleNode("Airfoils");
+                XmlNodeList grps = root.SelectNodes("grp");
+                foreach (XmlNode gn in grps)
+                {
+                    airfoilGroups.Add((gn as XmlElement).GetAttribute("Name"));
+                }
+                NotifyPropertyChanged("AirfoilGroups");
+            }
+        }
+
         private void LoadEditorParameters()
         {
             // load back the tool specific parameters
@@ -100,13 +329,37 @@ namespace Make3D.Dialogs
             // save the parameters for the tool
         }
 
+        private void SetProfiles(string grpName, List<string> names)
+        {
+            XmlNode root = airFoilDoc.SelectSingleNode("Airfoils");
+            names.Clear();
+            XmlNodeList grps = root.SelectNodes("grp");
+            foreach (XmlNode gn in grps)
+            {
+                if (grpName == (gn as XmlElement).GetAttribute("Name"))
+                {
+                    XmlNodeList nodeList = gn.SelectNodes("af");
+                    foreach (XmlNode nd in nodeList)
+                    {
+                        XmlElement el = nd as XmlElement;
+                        names.Add(el.GetAttribute("Name"));
+                    }
+                }
+            }
+        }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            LoadAirFoils();
+
+            RootGroup = airfoilGroups[0];
+
+            SelectedAirfoil = airfoilNames[0];
+
             LoadEditorParameters();
-            GenerateShape();
             UpdateCameraPos();
             MyModelGroup.Children.Clear();
-
+            GenerateShape();
             Redisplay();
         }
     }
