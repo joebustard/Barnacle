b6041a78ab5baa5340de7499e71c220f0f43a2c8
joebustard <joebustard@aol.com>
2021-08-10 23:51:41 +0100

    removed unused files

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.0.973")]
-[assembly: AssemblyFileVersion("1.0.0.973")]
+[assembly: AssemblyVersion("1.0.0.975")]
+[assembly: AssemblyFileVersion("1.0.0.975")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
