7f83c12d8892a646901691c4ce7bf1dc04afa4dd
joebustard <joebustard@aol.com>
2021-12-14 18:26:42 +0000

    Moved some templates around

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.0.553")]
-[assembly: AssemblyFileVersion("1.0.0.553")]
+[assembly: AssemblyVersion("1.0.0.556")]
+[assembly: AssemblyFileVersion("1.0.0.556")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
