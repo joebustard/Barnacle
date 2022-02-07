1df1f21f2b02bdc70e32691e13accffb386af131
joebustard <joebustard@aol.com>
2022-02-07 15:47:14 +0000

    Added some icons

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.0.892")]
-[assembly: AssemblyFileVersion("1.0.0.892")]
+[assembly: AssemblyVersion("1.0.0.902")]
+[assembly: AssemblyFileVersion("1.0.0.902")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
