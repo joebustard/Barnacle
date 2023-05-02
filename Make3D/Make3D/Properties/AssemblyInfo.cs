fb21c5c8519fdf912ac8501505db8f503490c05f
joebustard <joebustard@aol.com>
2023-05-02 10:03:59 +0100

    change version number to 1.0.4

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.3.1323")]
-[assembly: AssemblyFileVersion("1.0.3.1323")]
+[assembly: AssemblyVersion("1.0.4.1324")]
+[assembly: AssemblyFileVersion("1.0.4.1324")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
