b274f475f6c709dcf9817da842b0fbfd5f30c57f
joebustard <joebustard@aol.com>
2023-05-16 23:49:30 +0100

    more dev on path loft

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -1,9 +1,4 @@
 ﻿
-
-
-
-
-
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -20,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.4.1370")]
-[assembly: AssemblyFileVersion("1.0.4.1370")]
+[assembly: AssemblyVersion("1.0.4.1371")]
+[assembly: AssemblyFileVersion("1.0.4.1371")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
