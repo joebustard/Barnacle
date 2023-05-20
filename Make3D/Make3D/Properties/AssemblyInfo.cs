ba0c6c5501bd8aba0915f3486f34b5bbed1f8f7f
joebustard <joebustard@aol.com>
2023-05-20 10:46:56 +0100

    Update AssemblyInfo.cs

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -1,9 +1,4 @@
 ﻿
-
-
-
-
-
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -20,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.4.1418")]
-[assembly: AssemblyFileVersion("1.0.4.1418")]
+[assembly: AssemblyVersion("1.0.4.1419")]
+[assembly: AssemblyFileVersion("1.0.4.1419")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
