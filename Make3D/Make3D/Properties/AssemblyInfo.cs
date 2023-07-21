a4c6686b674622731d7aa757b148ff68f57508fb
joebustard <joseph.bustard@flowbird.group>
2023-07-21 16:33:14 +0100

    Restructured symbol font tool to enable code reuse

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.4.2021")]
-[assembly: AssemblyFileVersion("1.0.4.2021")]
+[assembly: AssemblyVersion("1.0.4.2024")]
+[assembly: AssemblyFileVersion("1.0.4.2024")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
--- Make3D/MakerLib/MakeSymbolFont.cs
+++ Make3D/MakerLib/MakeSymbolFont.cs
@@ -9,20 +9,20 @@ namespace MakerLib
 {
     public class SymbolFontMaker : MakerBase
     {
-
         private string symbolCode;
         private string symbolFontName;
 
         public SymbolFontMaker(string symbolCode, string fontName)
         {
-           
             this.symbolCode = symbolCode;
             this.symbolFontName = fontName;
         }
+
         public struct ResultDetails
         {
             public bool status;
         }
+
         public async Task<ResultDetails> GenerateAsync(Point3DCollection Vertices, Int32Collection Faces)
         {
             MakeSymbolUtils msu = new MakeSymbolUtils();
@@ -30,20 +30,14 @@ namespace MakerLib
             msu.Vertices = Vertices;
             if (!String.IsNullOrEmpty(symbolCode) && !String.IsNullOrEmpty(symbolFontName))
             {
-                await Task.Run(() => msu.GenerateSymbol(symbolCode, symbolFontName));       
+                await Task.Run(() => msu.GenerateSymbol(symbolCode, symbolFontName));
             }
             ResultDetails ret = new ResultDetails();
-            /*
-            ret.pnts = new List<Point3D>();
-            foreach(Point3D p in Vertices)
-            {
-                ret.pnts.Add(new Point3D(p.X, p.Y, p.Z));
-            }
-            ret.faces = Faces;
-            */
+
             ret.status = true;
             return ret;
         }
+
         public void Generate(Point3DCollection pnts, Int32Collection faces)
         {
             pnts.Clear();
