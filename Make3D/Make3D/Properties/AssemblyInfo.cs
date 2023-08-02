09c9e288647ab5088d8e64f95157d3be534f372d
joebustard <joseph.bustard@flowbird.group>
2023-08-02 18:49:29 +0100

    Some xaml style changes for flexipath control

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -1,4 +1,9 @@
 ﻿
+
+
+
+
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -15,7 +20,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.4.2093")]
-[assembly: AssemblyFileVersion("1.0.4.2093")]
+[assembly: AssemblyVersion("1.0.4.2096")]
+[assembly: AssemblyFileVersion("1.0.4.2096")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
