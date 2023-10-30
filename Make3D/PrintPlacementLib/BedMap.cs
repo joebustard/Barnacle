0b2ba7eb9ba261533ef8fc1342172ec750771b04
joebustard <joseph.bustard@flowbird.group>
2023-10-30 18:44:25 +0000

    Start new placement lib

---
+++ Make3D/PrintPlacementLib/BedMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintPlacementLib
+{
+    internal class BedMap
+    {
+        public bool[,] map;
+        private int numRows;
+        private int numCols;
+
+        public BedMap(int rows, int cols)
+        {
+            numRows = rows;
+            numCols = cols;
+            map = new bool[rows, cols];
+        }
+    }
+}
