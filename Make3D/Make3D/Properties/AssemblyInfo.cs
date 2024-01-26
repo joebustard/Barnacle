25a5a5157f695a99858757a4a76ac3fcdf0ebb0b
joebustard <joseph.bustard@flowbird.group>
2024-01-26 15:54:10 +0000

    More subdivision

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.5.3129")]
-[assembly: AssemblyFileVersion("1.0.5.3129")]
+[assembly: AssemblyVersion("1.0.5.3137")]
+[assembly: AssemblyFileVersion("1.0.5.3137")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
