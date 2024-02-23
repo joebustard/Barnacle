575c7be092529d00ef13f2b819630857e6e292a5
joebustard <joseph.bustard@flowbird.group>
2024-02-23 18:01:57 +0000

    More DIce primitive

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.5.3393")]
-[assembly: AssemblyFileVersion("1.0.5.3393")]
+[assembly: AssemblyVersion("1.0.5.3434")]
+[assembly: AssemblyFileVersion("1.0.5.3434")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
