9f11cb195819db23a0e90bdac23ee4b14cd0ad11
joebustard <joseph.bustard@flowbird.group>
2024-02-27 18:51:21 +0000

    start adding some cura json processing

---
+++ Make3D/Workflow/CuraDefinition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workflow
+{
+    public class CuraDefinition
+    {
+        public String name { get; set; }
+        public String version { get; set; }
+        public String inherits { get; set; }
+    }
+}
