22f17a0ef3494516f5a04aa6fc36119be9a935f9
joebustard <joebustard@aol.com>
2024-04-11 00:59:57 +0100

    more icons

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -1,9 +1,4 @@
 ﻿
-
-
-
-
-
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -20,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.5.3719")]
-[assembly: AssemblyFileVersion("1.0.5.3719")]
+[assembly: AssemblyVersion("1.0.5.3722")]
+[assembly: AssemblyFileVersion("1.0.5.3722")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
