4b3a8531409af69b84a5b25b0121464655b05d73
joebustard <joebustard@aol.com>
2024-04-14 22:58:43 +0100

    more changes to circular paste, more cura definition file

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -1,9 +1,4 @@
 ﻿
-
-
-
-
-
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -20,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.5.3760")]
-[assembly: AssemblyFileVersion("1.0.5.3760")]
+[assembly: AssemblyVersion("1.0.5.3775")]
+[assembly: AssemblyFileVersion("1.0.5.3775")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
