f039df8b11147ed5ce1e433c624a645d296b793f
joebustard <joebustard@aol.com>
2024-04-23 19:41:55 +0100

    modded xbar primitive to ensure its centre is at 0

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.5.3833")]
-[assembly: AssemblyFileVersion("1.0.5.3833")]
+[assembly: AssemblyVersion("1.0.5.3834")]
+[assembly: AssemblyFileVersion("1.0.5.3834")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
