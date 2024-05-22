600e603a7bd4e0f4eb8967698a626fb21046bc6a
joebustard <joebustard@aol.com>
2024-05-22 19:42:08 +0100

    correct gridsplitter changes

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.5.4046")]
-[assembly: AssemblyFileVersion("1.0.5.4046")]
+[assembly: AssemblyVersion("1.0.5.4049")]
+[assembly: AssemblyFileVersion("1.0.5.4049")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
