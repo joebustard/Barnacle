ea9862120f3765b97812cb4bf3fb7acbbfe4cb4e
joebustard <joebustard@aol.com>
2026-09-07 16:24:22 +0100

    Upgrade Framework version

--- Make3D/Make3D/Properties/AssemblyInfo.cs
+++ Make3D/Make3D/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.9.7186")]
-[assembly: AssemblyFileVersion("1.0.9.7186")]
+[assembly: AssemblyVersion("1.0.9.7187")]
+[assembly: AssemblyFileVersion("1.0.9.7187")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
